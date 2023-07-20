762585b70fdb24f925485fe751ab692fd9e6e7f0
Dusan Orlovic <dule@hey.com>
2023-07-20 10:18:11 +0200

    Add CI conf

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # https://github.com/duleorlovic/rails_capybara_selenium/blob/main/test/application_system_test_case.rb
+  if ENV["USE_HEADFULL_CHROME"].present?
+    # Use chrome only on local machine since CI will fail
+    # export USE_HEADFULL_CHROME=true
+    # rails test:system
+    # unset USE_HEADFULL_CHROME
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
 end
