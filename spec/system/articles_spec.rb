8404036d7496e1f502afe8bc2d9c44f51bc74df9
Dusan Orlovic <dule@hey.com>
2023-07-20 13:51:55 +0200

    Use driven_by before each spec

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :system do
-  before do
-    driven_by(:selenium)
-  end
-
   it "see articles page" do
     visit articles_path
     expect(page.body).to include  "Articles"
